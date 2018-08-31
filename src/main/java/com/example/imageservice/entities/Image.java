4c6022a62001d0d326ab508f4820fac658c20d6f
vipinraj <vipin.mscis3@iiitmk.ac.in>
2018-09-01 00:56:25 +0530

    first

---
+++ src/main/java/com/example/imageservice/entities/Image.java
@@ -0,0 +1,37 @@
+package com.example.imageservice.entities;
+
+public class Image {
+    Integer id;
+    String title;
+    String url;
+
+    public Image(Integer id, String title, String url) {
+        this.id = id;
+        this.title = title;
+        this.url = url;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
